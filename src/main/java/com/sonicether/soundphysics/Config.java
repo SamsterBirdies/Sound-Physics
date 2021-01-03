8b7fdd7cddb460a236cd79e7bc0da4309d16b464
SamsterBirdies <SamsterBirdies@users.noreply.github.com>
2021-01-02 20:51:13 -0800

    test

--- src/main/java/com/sonicether/soundphysics/Config.java
+++ src/main/java/com/sonicether/soundphysics/Config.java
@@ -117,22 +117,19 @@ public class Config {
 				"The global volume multiplier of all sounds.");
 		globalReverbGain = this.forgeConfig.getFloat("Global Reverb Gain", categoryGeneral, 1.0f, 0.1f, 2.0f,
 				"The global volume of simulated reverberations.");
-		globalReverbBrightness = this.forgeConfig.getFloat("Global Reverb Brightness", categoryGeneral, 1.0f, 0.1f,
-				2.0f,
+		globalReverbBrightness = this.forgeConfig.getFloat("Global Reverb Brightness", categoryGeneral, 1.0f, 0.1f, 2.0f,
 				"The brightness of reverberation. Higher values result in more high frequencies in reverberation. Lower values give a more muffled sound to the reverb.");
-		globalBlockAbsorption = this.forgeConfig.getFloat("Global Block Absorption", categoryGeneral, 1.0f, 0.1f, 4.0f,
+		globalBlockAbsorption = this.forgeConfig.getFloat("Global Block Absorption", categoryGeneral, 0.1f, 0.1f, 4.0f,
 				"The global amount of sound that will be absorbed when traveling through blocks.");
-		globalBlockReflectance = this.forgeConfig.getFloat("Global Block Reflectance", categoryGeneral, 1.0f, 0.1f,
-				4.0f,
+		globalBlockReflectance = this.forgeConfig.getFloat("Global Block Reflectance", categoryGeneral, 1.0f, 0.1f, 4.0f,
 				"The global amount of sound reflectance energy of all blocks. Lower values result in more conservative reverb simulation with shorter reverb tails. Higher values result in more generous reverb simulation with higher reverb tails.");
-		soundDistanceAllowance = this.forgeConfig.getFloat("Sound Distance Allowance", categoryGeneral, 4.0f, 1.0f,
-				6.0f,
+		soundDistanceAllowance = this.forgeConfig.getFloat("Sound Distance Allowance", categoryGeneral, 6.0f, 1.0f, 6.0f,
 				"Minecraft won't allow sounds to play past a certain distance. This parameter is a multiplier for how far away a sound source is allowed to be in order for it to actually play. Values too high can cause polyphony issues.");
 		airAbsorption = this.forgeConfig.getFloat("Air Absorption", categoryGeneral, 1.0f, 0.0f, 5.0f,
 				"A value controlling the amount that air absorbs high frequencies with distance. A value of 1.0 is physically correct for air with normal humidity and temperature. Higher values mean air will absorb more high frequencies with distance. 0 disables this effect.");
 		snowAirAbsorptionFactor = this.forgeConfig.getFloat("Max Snow Air Absorption Factor", categoryGeneral, 5.0f, 0.0f, 10.0f,
 				"The maximum air absorption factor when it's snowing. The real absorption factor will depend on the snow's intensity. Set to 1 or lower to disable");
-		underwaterFilter = this.forgeConfig.getFloat("Underwater Filter", categoryGeneral, 0.8f, 0.0f, 1.0f,
+		underwaterFilter = this.forgeConfig.getFloat("Underwater Filter", categoryGeneral, 0.0f, 0.0f, 1.0f,
 				"How much sound is filtered when the player is underwater. 0.0 means no filter. 1.0 means fully filtered.");
 		noteBlockEnable = this.forgeConfig.getBoolean("Affect Note Blocks", categoryGeneral, true,
 				"If true, note blocks will be processed.");
@@ -140,34 +137,33 @@ public class Config {
 				"How far the rays should be traced.");
 		volumeMulOnlyAffected = this.forgeConfig.getBoolean("Volume Multiplier Only On Affected", categoryGeneral, true,
 				"If true, the global volume multiplier will only be applied to affected sounds (so not to the ui sounds for example).");
-		globalEchoMultiplier = this.forgeConfig.getFloat("Global Echo Multiplier", categoryGeneral, 1.0f, 0.0f, 2.0f,
+		globalEchoMultiplier = this.forgeConfig.getFloat("Global Echo Multiplier", categoryGeneral, 1.0f, 0.0f, 10.0f,
 				"The global volume multiplier of the echos, put to 0 to disable echos all together");
 
 		// performance
 		skipRainOcclusionTracing = this.forgeConfig.getBoolean("Skip Rain Occlusion Tracing", categoryPerformance, true,
 				"If true, rain sound sources won't trace for sound occlusion. This can help performance during rain.");
-		environmentEvaluationRays = this.forgeConfig.getInt("Environment Evaluation Rays", categoryPerformance, 32, 8,
-				64,
+		environmentEvaluationRays = this.forgeConfig.getInt("Environment Evaluation Rays", categoryPerformance, 32, 8, 64,
 				"The number of rays to trace to determine reverberation for each sound source. More rays provides more consistent tracing results but takes more time to calculate. Decrease this value if you experience lag spikes when sounds play.");
 		simplerSharedAirspaceSimulation = this.forgeConfig.getBoolean("Simpler Shared Airspace Simulation",
 				categoryPerformance, false,
 				"If true, enables a simpler technique for determining when the player and a sound source share airspace. Might sometimes miss recognizing shared airspace, but it's faster to calculate.");
 
 		// material properties
-		stoneReflectivity = this.forgeConfig.getFloat("Stone Reflectivity", categoryMaterialProperties, 0.95f, 0.0f,
-				1.0f, "Sound reflectivity for stone blocks.");
-		woodReflectivity = this.forgeConfig.getFloat("Wood Reflectivity", categoryMaterialProperties, 0.7f, 0.0f, 1.0f,
+		stoneReflectivity = this.forgeConfig.getFloat("Stone Reflectivity", categoryMaterialProperties, 0.95f, 0.0f, 1.0f, 
+				"Sound reflectivity for stone blocks.");
+		woodReflectivity = this.forgeConfig.getFloat("Wood Reflectivity", categoryMaterialProperties, 0.5f, 0.0f, 1.0f,
 				"Sound reflectivity for wooden blocks.");
-		groundReflectivity = this.forgeConfig.getFloat("Ground Reflectivity", categoryMaterialProperties, 0.3f, 0.0f,
-				1.0f, "Sound reflectivity for ground blocks (dirt, gravel, etc).");
-		plantReflectivity = this.forgeConfig.getFloat("Foliage Reflectivity", categoryMaterialProperties, 0.2f, 0.0f,
-				1.0f, "Sound reflectivity for foliage blocks (leaves, grass, etc.).");
-		metalReflectivity = this.forgeConfig.getFloat("Metal Reflectivity", categoryMaterialProperties, 0.97f, 0.0f,
-				1.0f, "Sound reflectivity for metal blocks.");
-		glassReflectivity = this.forgeConfig.getFloat("Glass Reflectivity", categoryMaterialProperties, 0.5f, 0.0f,
-				1.0f, "Sound reflectivity for glass blocks.");
-		clothReflectivity = this.forgeConfig.getFloat("Cloth Reflectivity", categoryMaterialProperties, 0.25f, 0.0f,
-				1.0f, "Sound reflectivity for cloth blocks (carpet, wool, etc).");
+		groundReflectivity = this.forgeConfig.getFloat("Ground Reflectivity", categoryMaterialProperties, 0.3f, 0.0f, 1.0f, 
+				"Sound reflectivity for ground blocks (dirt, gravel, etc).");
+		plantReflectivity = this.forgeConfig.getFloat("Foliage Reflectivity", categoryMaterialProperties, 0.2f, 0.0f, 1.0f, 
+				"Sound reflectivity for foliage blocks (leaves, grass, etc.).");
+		metalReflectivity = this.forgeConfig.getFloat("Metal Reflectivity", categoryMaterialProperties, 0.97f, 0.0f, 1.0f, 
+				"Sound reflectivity for metal blocks.");
+		glassReflectivity = this.forgeConfig.getFloat("Glass Reflectivity", categoryMaterialProperties, 0.5f, 0.0f, 1.0f, 
+				"Sound reflectivity for glass blocks.");
+		clothReflectivity = this.forgeConfig.getFloat("Cloth Reflectivity", categoryMaterialProperties, 0.25f, 0.0f, 1.0f, 
+				"Sound reflectivity for cloth blocks (carpet, wool, etc).");
 		sandReflectivity = this.forgeConfig.getFloat("Sand Reflectivity", categoryMaterialProperties, 0.2f, 0.0f, 1.0f,
 				"Sound reflectivity for sand blocks.");
 		snowReflectivity = this.forgeConfig.getFloat("Snow Reflectivity", categoryMaterialProperties, 0.2f, 0.0f, 1.0f,
